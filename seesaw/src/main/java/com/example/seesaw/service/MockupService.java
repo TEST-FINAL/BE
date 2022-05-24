60b8845dbb6388ae981d6764a03a814ed793f224
hmhmchm <hyeonminchoi5@gmail.com>
2022-05-24 23:39:45 +0900

    *feat : 5/24 게임 + 수정사항

--- seesaw/src/main/java/com/example/seesaw/service/MockupService.java
+++ seesaw/src/main/java/com/example/seesaw/service/MockupService.java
@@ -1,5 +1,6 @@
 package com.example.seesaw.service;
 
+import com.example.seesaw.dto.CrossWordRequestDto;
 import com.example.seesaw.dto.CrossWordResponseDto;
 import com.example.seesaw.model.Crossword;
 import com.example.seesaw.model.QuizNum;
@@ -14,23 +15,23 @@ import java.util.List;
 @Service
 public class MockupService {
 
-    private final CrosswordRepository crosswordRepository;
-
-    // 목업용 게임판 quizId 1번 전체 조회
-    public List<CrossWordResponseDto> getWord() {
-        Long id = 1L;
-        QuizNum quizNum = new QuizNum(id); // 1로 고정
-        List<Crossword> crosswords = crosswordRepository.findAllByQuizNum(quizNum);
-        // 단어 시작 첫 좌표, 방향, 글자 수, 정답들 담을 리스트
-        List<CrossWordResponseDto> crossWordResponseDtos = new ArrayList<>();
-
-        for (Crossword c:crosswords
-             ) {
-            boolean pass = false;
-            crossWordResponseDtos.add(new CrossWordResponseDto(c, pass));
-        }
-
-        return crossWordResponseDtos;
-    }
+//    private final CrosswordRepository crosswordRepository;
+//
+//    // 목업용 게임판 quizId 1번 전체 조회
+//    public List<CrossWordResponseDto> getWord() {
+//        Long id = 1L;
+//        QuizNum quizNum = new QuizNum(id); // 1로 고정
+//        List<Crossword> crosswords = crosswordRepository.findAllByQuizNum(quizNum);
+//        // 단어 시작 첫 좌표, 방향, 글자 수, 정답들 담을 리스트
+//        List<CrossWordResponseDto> crossWordResponseDtos = new ArrayList<>();
+//
+//        for (Crossword c:crosswords
+//             ) {
+//            boolean pass = false;
+//            crossWordResponseDtos.add(new CrossWordResponseDto(c, pass));
+//        }
+//
+//        return crossWordResponseDtos;
+//    }
 
 }
---
+++ seesaw/src/main/java/com/example/seesaw/test.sql
@@ -0,0 +1,11 @@
+# insert into crossword (id, contents, is_oriental, word, word_count, x, y, quiz_num_id) VALUE (1,'내용',true,'단어',2,2,2,1)
+# insert into quiz_num (id, quiz_number, crossword_id) VALUE (1,1,1)
+# alter table post drop user
+# insert into post_image (id, image_url, postid) VALUE (6, 'image3x.url', 3)
+# insert into trouble (id, answer, content, question, title, view_count, user_id)
+# VALUE (4, 'Z세대', '내용3','M세대','제목3',10,3)
+
+# delete from post_comment where id ='5'
+# delete from post_scrap where id = '24'
+
+# truncate post;
