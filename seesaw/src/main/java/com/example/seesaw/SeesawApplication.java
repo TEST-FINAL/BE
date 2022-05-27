acad6dcb0852a9de97e6c02aace1dd68f7dc1d08
hmhmchm <hyeonminchoi5@gmail.com>
2022-05-28 00:05:48 +0900

    rename : 패키지 정리

--- seesaw/src/main/java/com/example/seesaw/SeesawApplication.java
+++ seesaw/src/main/java/com/example/seesaw/SeesawApplication.java
@@ -1,14 +1,11 @@
 package com.example.seesaw;
 
-import com.example.seesaw.game.Board;
-import com.example.seesaw.game.WordsGetter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PostConstruct;
-import java.util.Scanner;
 import java.util.TimeZone;
 
 @EnableScheduling
@@ -18,7 +15,9 @@ public class SeesawApplication {
 
 
     public static void main(String[] args) {
+
         SpringApplication.run(SeesawApplication.class, args);
+
     }
 
     @PostConstruct
